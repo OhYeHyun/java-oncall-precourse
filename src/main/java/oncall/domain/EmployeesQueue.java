95bb1061af8afea7e69edc5a19879687967445fb
OhYeHyun <ohyhohyl@gmail.com>
2024-12-13 18:05:41 +0900

    feat(EmployeesQueue): EmployeesQueue 객체 생성

---
+++ src/main/java/oncall/domain/EmployeesQueue.java
@@ -0,0 +1,26 @@
+package oncall.domain;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public class EmployeesQueue {
+    private final List<String> employees;
+    private int position = 0;
+
+    public EmployeesQueue(List<String> employees) {
+        this.employees = employees;
+    }
+
+    public boolean isMatch(String targetEmployee) {
+        return Objects.equals(targetEmployee, employees.get(position));
+    }
+
+    public void switchEmployee() {
+        Collections.swap(employees, position, (position + 1) % employees.size());
+    }
+
+    public void addPosition() {
+        position = (position + 1) % employees.size();
+    }
+}
