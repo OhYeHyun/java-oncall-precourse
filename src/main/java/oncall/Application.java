2992d99e18092e594d5d6dcf06d24051b62b82eb
OhYeHyun <ohyhohyl@gmail.com>
2024-12-13 20:17:04 +0900

    feat(Application): 프로그램 실행

--- src/main/java/oncall/Application.java
+++ src/main/java/oncall/Application.java
@@ -1,7 +1,36 @@
 package oncall;
 
+import oncall.controller.OnCallController;
+import oncall.domain.Calendar;
+import oncall.domain.DayOfWeek;
+import oncall.domain.MonthlySchedule;
+import oncall.service.CalendarService;
+import oncall.service.InitializeService;
+import oncall.service.OnCall;
+import oncall.service.QueueService;
+import oncall.validator.BusinessValidator;
+import oncall.view.InputView;
+import oncall.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Calendar calendar = new Calendar();
+        InitializeService initializeService = new InitializeService(calendar);
+
+        DayOfWeek dayOfWeek = new DayOfWeek();
+        CalendarService calendarService = new CalendarService(dayOfWeek);
+
+        MonthlySchedule monthlySchedule = new MonthlySchedule();
+        QueueService queueService = new QueueService(monthlySchedule);
+
+        OnCall onCall = new OnCall(initializeService, calendarService, queueService);
+
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        BusinessValidator businessValidator = new BusinessValidator();
+
+        OnCallController controller = new OnCallController(onCall, inputView, outputView, businessValidator);
+        controller.run();
     }
 }
